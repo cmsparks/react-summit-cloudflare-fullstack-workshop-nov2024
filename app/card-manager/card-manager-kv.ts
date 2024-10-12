91e7c5eaba3164b23ff306d3c64fc9bf28a03358
Christian Sparks <csparks@cloudflare.com>
2024-10-12 12:49:16 -0500

    implement kv and r2 card manager
    (exercise 02 solution)
    (exercise 03 start)

--- app/card-manager/card-manager-kv.ts
+++ app/card-manager/card-manager-kv.ts
@@ -1,3 +1,8 @@
+import { isCard } from "./utils";
+
+const IMAGE_KEY_PREFIX = "/image";
+const DATA_KEY_PREFIX = "/data";
+
 /**
  * Trading card manager class that wraps KV, and Workers AI. Handles all of our "business" logic
  */
@@ -7,26 +12,87 @@ export class CardManagerKV implements CardManager {
   async generateAndSaveCard(
     card: Pick<Card, "title" | "description">
   ): Promise<string> {
-    // TODO
-    throw new Error("Unimplemented");
+    const key = crypto.randomUUID();
+
+    const cardData = await this.generateCardArt(card);
+
+    // we don't know the length of the readable stream returned from ai.run(),
+    // so we need to read it all into a buffer so we can use it in r2Binding.put()
+
+    // 1) read the entire ReadableStream to get the data and length
+    const reader = cardData.getReader();
+    const buffer = [];
+    let totalLength = 0;
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) break;
+      buffer.push(value);
+      totalLength += value.byteLength;
+    }
+
+    // 2) convert to a statically sized Uint8Array()
+    const arrayBuffer = new Uint8Array(totalLength);
+    let offset = 0;
+    for (const value of buffer) {
+      arrayBuffer.set(value, offset);
+      offset += value.byteLength;
+    }
+
+    await Promise.all([
+      this.env.KV.put(`${IMAGE_KEY_PREFIX}/${key}`, arrayBuffer),
+      this.env.KV.put(`${DATA_KEY_PREFIX}/${key}`, JSON.stringify(card)),
+    ]);
+
+    return key;
   }
 
   async generateCardArt(
     card: Pick<Card, "title" | "description">
   ): Promise<ReadableStream<Uint8Array>> {
-    // TODO
-    throw new Error("Unimplemented");
+    // create prompt
+    const input = {
+      prompt: [
+        `Based on the following title and description, generate card artwork for a trading card`,
+        `title: ${card.title}`,
+        `description: ${card.description}`,
+      ].join("\n"),
+    };
+
+    // generate image data from aiBinding
+    const imageData = await this.env.AI.run(
+      "@cf/stabilityai/stable-diffusion-xl-base-1.0",
+      input
+    );
+
+    return imageData;
   }
 
   async getCard(cardId: string): Promise<Card | null> {
-    // TODO
-    throw new Error("Unimplemented");
+    const partialCard = await this.env.KV.get<Omit<Card, "imageUrl">>(
+      `${DATA_KEY_PREFIX}/${cardId}`,
+      "json"
+    );
+
+    if (!partialCard) {
+      // key not found
+      return null;
+    }
+
+    const card = {
+      ...partialCard,
+      imageUrl: `/image/${cardId}`,
+    };
+
+    if (!isCard(card)) {
+      throw new Error("Invalid card returned from KV");
+    }
+
+    return card;
   }
 
   async getCardImage(
     cardId: string
   ): Promise<ReadableStream<Uint8Array> | null> {
-    // TODO
-    throw new Error("Unimplemented");
+    return await this.env.KV.get(`${IMAGE_KEY_PREFIX}/${cardId}`, "stream");
   }
 }
