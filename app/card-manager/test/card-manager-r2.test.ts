e6c65cd73120d4f3497ba7b07d7925e51de186ca
Christian Sparks <csparks@cloudflare.com>
2024-10-12 12:49:16 -0500

    implement kv and r2 card manager: generateCardImage
    (exercise 02 solution 1/4)

--- app/card-manager/test/card-manager-r2.test.ts
+++ app/card-manager/test/card-manager-r2.test.ts
@@ -1,4 +1,4 @@
-import { afterEach, describe, it, vi } from "vitest";
+import { afterEach, expect, describe, it, vi } from "vitest";
 import { env } from "cloudflare:test";
 import { CardManagerR2 } from "../card-manager-r2";
 
@@ -14,7 +14,31 @@ describe("test CardManagerR2 class", () => {
   });
 
   it("generateCardImage()", async () => {
-    // TODO
+    const title = "test title";
+    const description = "test description";
+
+    const imageData = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8]);
+    const expectedPrompt = {
+      prompt: [
+        `Based on the following title and description, generate card artwork for a trading card`,
+        `title: ${title}`,
+        `description: ${description}`,
+      ].join("\n"),
+    };
+    const mockAI = vi.spyOn(env.AI, "run").mockResolvedValueOnce(imageData);
+
+    const cardData = await cardManager.generateCardImage({
+      title,
+      description,
+    });
+
+    expect(cardData).toStrictEqual(imageData);
+
+    expect(mockAI).toHaveBeenCalledWith(
+      "@cf/stabilityai/stable-diffusion-xl-base-1.0",
+      expectedPrompt
+    );
+    expect(mockAI).toHaveBeenCalledOnce();
   });
 
   it("getCard(): null card", async () => {
