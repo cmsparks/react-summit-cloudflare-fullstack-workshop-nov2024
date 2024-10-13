be454483ce8862bb0a476ac54aa9143e34896342
Dario Piotrowicz <dario@cloudflare.com>
2024-10-13 15:37:52 +0100

    implement e2e test for card generation
    (exercise 04 solution)

--- app/components/cardForm.tsx
+++ app/components/cardForm.tsx
@@ -8,13 +8,17 @@ export default function CardForm({ submitting }: Props) {
   return (
     <Form className="card-form" method="post">
       {submitting && (
-        <div className="card-form__loading">
+        <div
+          className="card-form__loading"
+          data-testid="card-loading-indicator"
+        >
           <div className="loader"></div>
         </div>
       )}
-      <div className="card">
-        <div className="card__image"></div>
+      <div className="card" data-testid="card">
+        <div className="card__image" data-testid="card-image-placeholder"></div>
         <input
+          data-testid="card-title-input"
           className="card__title card__title--input"
           type="text"
           name="card-title"
@@ -24,6 +28,7 @@ export default function CardForm({ submitting }: Props) {
           required
         />
         <textarea
+          data-testid="card-description-input"
           className="card__description card__description--input"
           name="card-description"
           id="card-description"
@@ -32,7 +37,11 @@ export default function CardForm({ submitting }: Props) {
           required
         ></textarea>
       </div>
-      <button disabled={submitting} className="btn btn--generate">
+      <button
+        data-testid="card-generate-btn"
+        disabled={submitting}
+        className="btn btn--generate"
+      >
         Generate
       </button>
     </Form>
